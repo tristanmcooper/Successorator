73dcc8948a795008ac38747fef04d2daabbc3db3
Eric Wang <ericwang@Erics-MacBook-Pro-2.local>
2024-03-10 15:47:46 -0700

    some changes

--- app/src/main/java/edu/ucsd/cse110/successorator/app/data/db/SuccessoratorDatabase.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/app/data/db/SuccessoratorDatabase.java
@@ -3,11 +3,8 @@ package edu.ucsd.cse110.successorator.app.data.db;
 import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-import androidx.room.migration.Migration;
-import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {GoalEntity.class}, version = 1)
 public abstract class SuccessoratorDatabase extends RoomDatabase {
     public abstract GoalDao goalDao();
-    // Migration code
 }
